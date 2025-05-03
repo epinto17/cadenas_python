3348365ee6ec92dc4b4b8923590c5c8ea35b6dde
epinto17 <pinto.ervin17@gmail.com>
2025-05-02 22:14:13 -0600

    Menu cadenas

---
+++ Cadenas.py
@@ -0,0 +1,18 @@
+def menu():
+    print ("Ejercicios con Cadenas")
+    print ("""
+        1. Ejercicio 1
+        2. Ejercicio 2
+        3. Ejercicio 3
+        4. Ejercicio 4
+        5. Ejercicio 5
+        6. Ejercicio 6
+        7. Ejercicio 7
+        8. Ejercicio 8
+        9. Ejercicio 9
+        10. Ejercicio 10
+    """)
+
+    opcion = int(input("Seleccione un programa: "))
+
+menu()
